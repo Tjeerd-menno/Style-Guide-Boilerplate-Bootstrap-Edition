9b66c64f2cd5a6d5b20bf69cb6307548d6f8573d
unknown <Tjeerd.Douma@T5328.defriesland.nl>
2014-09-02 12:34:11 +0200

    Without bootstrap.css

--- index.php
+++ index.php
@@ -126,7 +126,6 @@ echo '<li role="presentation" class="divider"></li><li role="presentation" class
 <meta name="viewport" content="width=device-width">
 <!-- Style Guide Boilerplate Styles -->
 <link rel="stylesheet" href="css/styleguide.css">
-<link rel="stylesheet" href="css/bootstrap.css">
 
 <!-- Replace below stylesheet with your own stylesheet -->
 <link rel="stylesheet" href="css/dfz.css">
